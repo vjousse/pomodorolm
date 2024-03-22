ff84fd789bedaf67d960c8d27431ff6cae27948e
Vincent Jousse <vincent@jousse.org>
2024-03-22 11:44:31 +0100

    Add footer

--- src/Main.elm
+++ src/Main.elm
@@ -36,81 +36,151 @@ update msg model =
             model - 1
 
 
+dialView : Html Msg
+dialView =
+    div [ class "dial-wrapper" ]
+        [ p [ class "dial-time" ] [ text "25:00" ]
+        , p [ class "dial-label" ] [ text "Focus" ]
+        , svg
+            [ SvgAttr.version "1.2"
+            , SvgAttr.baseProfile "tiny"
+            , SvgAttr.id "Layer_1"
+            , SvgAttr.x "0px"
+            , SvgAttr.y "0px"
+            , SvgAttr.viewBox "0 0 230 230"
+            , SvgAttr.xmlSpace "preserve"
+            , SvgAttr.width "220"
+            , SvgAttr.height "220"
+            , SvgAttr.class "dial-fill dial-fill--work"
+            ]
+            [ path
+                [ SvgAttr.fill "none"
+                , SvgAttr.strokeWidth "10"
+                , SvgAttr.strokeLinecap "round"
+                , SvgAttr.strokeMiterlimit "10"
+                , SvgAttr.d "M115,5c60.8,0,110,49.2,110,110s-49.2,110-110,110S5,175.8,5,115S54.2,5,115,5"
+                , SvgAttr.strokeDasharray "691.3321533203125"
+                , SvgAttr.style "stroke-dashoffset: 0px;"
+                ]
+                []
+            ]
+        , svg
+            [ SvgAttr.version "1.2"
+            , SvgAttr.baseProfile "tiny"
+            , SvgAttr.id "Layer_1"
+            , SvgAttr.x "0px"
+            , SvgAttr.y "0px"
+            , SvgAttr.viewBox "0 0 230 230"
+            , SvgAttr.xmlSpace "preserve"
+            , SvgAttr.width "220"
+            , SvgAttr.height "220"
+            , SvgAttr.class "dial-bg"
+            ]
+            [ path
+                [ SvgAttr.fill "none"
+                , SvgAttr.strokeWidth "2"
+                , SvgAttr.strokeLinecap "round"
+                , SvgAttr.strokeMiterlimit "10"
+                , SvgAttr.d "M115,5c60.8,0,110,49.2,110,110s-49.2,110-110,110S5,175.8,5,115S54.2,5,115,5"
+                ]
+                []
+            ]
+        ]
+
+
+playPauseView : Html Msg
+playPauseView =
+    section [ class "container", class "button-wrapper" ]
+        [ div [ class "button" ]
+            [ div [ class "button-icon-wrapper" ]
+                [ svg
+                    [ SvgAttr.version "1.2"
+                    , SvgAttr.baseProfile "tiny"
+                    , SvgAttr.id "Layer_1"
+                    , SvgAttr.x "0px"
+                    , SvgAttr.y "0px"
+                    , SvgAttr.viewBox "0 0 7.6 15"
+                    , SvgAttr.xmlSpace "preserve"
+                    , SvgAttr.height "15px"
+                    , SvgAttr.class "icon--start"
+                    ]
+                    [ Svg.polygon
+                        [ attribute "data-v-04292d65" ""
+                        , SvgAttr.fill "var(--color-foreground)"
+                        , SvgAttr.points "0,0 0,15 7.6,7.4 "
+                        ]
+                        []
+                    ]
+                ]
+            ]
+        ]
+
+
+footerView : Html Msg
+footerView =
+    section [ class "container", class "footer" ]
+        [ div [ class "round-wrapper" ]
+            [ p [] [ text "1/4" ]
+            , p [ class "text-button", title "Reset current round" ] [ text "Reset" ]
+            ]
+        , div [ class "icon-group", style "position" "absolute", style "right" "0px" ]
+            [ div [ class "icon-wrapper", class "icon-wrapper--double--left", title "Skip the current round" ]
+                [ svg
+                    [ SvgAttr.version "1.2"
+                    , SvgAttr.baseProfile "tiny"
+                    , SvgAttr.id "Layer_1"
+                    , SvgAttr.x "0px"
+                    , SvgAttr.y "0px"
+                    , SvgAttr.viewBox "0 0 8 12"
+                    , SvgAttr.xmlSpace "preserve"
+                    , SvgAttr.height "15px"
+                    , SvgAttr.class "Icon--skip"
+                    ]
+                    [ Svg.polygon
+                        [ SvgAttr.fill "var(--color-background-lightest)"
+                        , SvgAttr.points "0,0 0,12 6.1,5.9"
+                        ]
+                        []
+                    , Svg.rect
+                        [ SvgAttr.x "6.9"
+                        , SvgAttr.y "0"
+                        , SvgAttr.fill "var(--color-background-lightest)"
+                        , SvgAttr.width "1.1"
+                        , SvgAttr.height "12"
+                        ]
+                        []
+                    ]
+                ]
+            , div [ class "icon-wrapper", class "icon-wrapper--double--right", title "Mute" ]
+                [ svg
+                    [ SvgAttr.version "1.2"
+                    , SvgAttr.id "Layer_1"
+                    , SvgAttr.x "0px"
+                    , SvgAttr.y "0px"
+                    , SvgAttr.viewBox "0 0 12.3 12"
+                    , SvgAttr.xmlSpace "preserve"
+                    , SvgAttr.height "15px"
+                    , SvgAttr.class "Icon--mute"
+                    , SvgAttr.baseProfile "tiny"
+                    ]
+                    [ path
+                        [ attribute "data-v-b9a0799a" ""
+                        , SvgAttr.fill "var(--color-background-lightest)"
+                        , SvgAttr.d "M0,3.9v4.1h2.7l3.4,3.4V0.5L2.7,3.9H0z M9.2,6c0-1.2-0.7-2.3-1.7-2.8v5.5C8.5,8.3,9.2,7.2,9.2,6z M7.5,0v1.4 c2,0.6,3.4,2.4,3.4,4.6s-1.4,4-3.4,4.6V12c2.7-0.6,4.8-3.1,4.8-6S10.3,0.6,7.5,0z"
+                        ]
+                        []
+                    ]
+                ]
+            ]
+        ]
+
+
 timerView : Html Msg
 timerView =
     div [ class "timer-wrapper" ]
-        [ div [ class "dial-wrapper" ]
-            [ p [ class "dial-time" ] [ text "25:00" ]
-            , p [ class "dial-label" ] [ text "Focus" ]
-            , svg
-                [ SvgAttr.version "1.2"
-                , SvgAttr.baseProfile "tiny"
-                , SvgAttr.id "Layer_1"
-                , SvgAttr.x "0px"
-                , SvgAttr.y "0px"
-                , SvgAttr.viewBox "0 0 230 230"
-                , SvgAttr.xmlSpace "preserve"
-                , SvgAttr.width "220"
-                , SvgAttr.height "220"
-                , SvgAttr.class "dial-fill dial-fill--work"
-                ]
-                [ path
-                    [ SvgAttr.fill "none"
-                    , SvgAttr.strokeWidth "10"
-                    , SvgAttr.strokeLinecap "round"
-                    , SvgAttr.strokeMiterlimit "10"
-                    , SvgAttr.d "M115,5c60.8,0,110,49.2,110,110s-49.2,110-110,110S5,175.8,5,115S54.2,5,115,5"
-                    , SvgAttr.strokeDasharray "691.3321533203125"
-                    , SvgAttr.style "stroke-dashoffset: 0px;"
-                    ]
-                    []
-                ]
-            , svg
-                [ SvgAttr.version "1.2"
-                , SvgAttr.baseProfile "tiny"
-                , SvgAttr.id "Layer_1"
-                , SvgAttr.x "0px"
-                , SvgAttr.y "0px"
-                , SvgAttr.viewBox "0 0 230 230"
-                , SvgAttr.xmlSpace "preserve"
-                , SvgAttr.width "220"
-                , SvgAttr.height "220"
-                , SvgAttr.class "dial-bg"
-                ]
-                [ path
-                    [ SvgAttr.fill "none"
-                    , SvgAttr.strokeWidth "2"
-                    , SvgAttr.strokeLinecap "round"
-                    , SvgAttr.strokeMiterlimit "10"
-                    , SvgAttr.d "M115,5c60.8,0,110,49.2,110,110s-49.2,110-110,110S5,175.8,5,115S54.2,5,115,5"
-                    ]
-                    []
-                ]
-            ]
-        , section [ class "container", class "button-wrapper" ]
-            [ div [ class "button" ]
-                [ div [ class "button-icon-wrapper" ]
-                    [ svg
-                        [ SvgAttr.version "1.2"
-                        , SvgAttr.baseProfile "tiny"
-                        , SvgAttr.id "Layer_1"
-                        , SvgAttr.x "0px"
-                        , SvgAttr.y "0px"
-                        , SvgAttr.viewBox "0 0 7.6 15"
-                        , SvgAttr.xmlSpace "preserve"
-                        , SvgAttr.height "15px"
-                        , SvgAttr.class "icon--start"
-                        ]
-                        [ Svg.polygon
-                            [ attribute "data-v-04292d65" ""
-                            , SvgAttr.fill "var(--color-foreground)"
-                            , SvgAttr.points "0,0 0,15 7.6,7.4 "
-                            ]
-                            []
-                        ]
-                    ]
-                ]
-            ]
+        [ dialView
+        , playPauseView
+        , footerView
         ]
 
 
