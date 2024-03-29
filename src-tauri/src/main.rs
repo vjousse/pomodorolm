9127f12bc80688324b2d68db5dd1bddc9c0d930d
Vincent Jousse <vincent@jousse.org>
2024-03-29 14:56:39 +0100

    Add tauri

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,21 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri::{CustomMenuItem, SystemTray, SystemTrayMenu, SystemTrayMenuItem};
+
+fn main() {
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let hide = CustomMenuItem::new("hide".to_string(), "Hide");
+
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(quit)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(hide);
+
+    let system_tray = SystemTray::new().with_menu(tray_menu);
+
+    tauri::Builder::default()
+        .system_tray(system_tray)
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
---
+++ src-ts/main.ts
@@ -0,0 +1,44 @@
+import "../main.css";
+
+import { Elm } from "../src-elm/Main.elm";
+
+if (process.env.NODE_ENV === "development") {
+  const ElmDebugTransform = await import("elm-debug-transformer");
+
+  ElmDebugTransform.register({
+    simple_mode: true,
+  });
+}
+
+const root = document.querySelector("#app div");
+const app = Elm.Main.init({ node: root });
+
+app.ports.playSound.subscribe(function (soundElementId: string) {
+  let audioPlayer: HTMLVideoElement = document.getElementById(
+    soundElementId
+  ) as HTMLVideoElement;
+
+  audioPlayer.play();
+});
+
+/*
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    <h1>Vite + TypeScript</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`;
+
+setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+*/
