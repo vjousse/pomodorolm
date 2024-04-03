7d50fa7b36976bbee9689041dcdc00da40578f6a
Vincent Jousse <vincent@jousse.org>
2024-04-03 18:44:36 +0200

    Remove unused file

--- js/app.js
+++
@@ -1,6 +0,0 @@
-import { Elm } from "../src/Main.elm";
-var app = Elm.Main.init({ node: document.getElementById("elm-app") });
-
-app.ports.playSound.subscribe(function (soundElementId) {
-  document.getElementById(soundElementId).play();
-});
