89e584543fa5237dd272d4f2baf572772dda28fc
Vincent Jousse <vincent@jousse.org>
2024-09-06 16:31:50 +0200

    chore: add flatpak build (#78)
    * chore: add flatpak builds
    
    * fix: add debug for flatpak
    
    * fix: flatpak tray icon display
    
    * fix: don't use absolute path
    
    * chore: prepare flatpak submission

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -500,7 +500,19 @@ async fn change_icon(
             );
 
             if let Some(tray) = app_handle.tray_by_id("app-tray") {
-                let _ = tray.set_icon(tauri::image::Image::from_path(icon_path_buf).ok());
+                let icon_path = tauri::image::Image::from_path(icon_path_buf.clone()).ok();
+
+                // Don't let tauri choose where to store the temp icon path. Setting it allows the tray icon to work
+                // properly in sandboxes env like Flatpak where we can share XDG_DATA_HOME between the host and the sandboxed env
+                // libappindicator will add the full path of the icon to the dbus message when changing it,
+                // so the path needs to be the same between the host and the sandboxed env
+                let local_data_path = app_handle
+                    .path()
+                    .resolve("tray-icon", BaseDirectory::AppLocalData)
+                    .unwrap();
+
+                let _ = tray.set_temp_dir_path(Some(local_data_path));
+                let _ = tray.set_icon(icon_path);
             }
         }
         Err(e) => eprintln!("Unable to get app_data_dir for icon: {:?}.", e),
