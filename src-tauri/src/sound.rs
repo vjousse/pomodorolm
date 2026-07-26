794aa8282030a40546cd9025d6f99815083890a5
Vincent Jousse <vincent@jousse.org>
2026-07-26 17:56:55 +0200

    chore: update to latest rodio 0.22.2 (#295)

--- src-tauri/src/sound.rs
+++ src-tauri/src/sound.rs
@@ -1,19 +1,20 @@
-use rodio::{Decoder, OutputStream, Sink};
+use rodio::{Decoder, DeviceSinkBuilder};
 use std::fs::File;
 use std::io::BufReader;
 
 pub fn play_sound_file(resource_path: &str, volume: f32) -> Result<(), Box<dyn std::error::Error>> {
     // Get a output stream handle to the default physical sound device
-    let (_stream, stream_handle) = OutputStream::try_default()?;
-    let sink = Sink::try_new(&stream_handle)?;
-    sink.set_volume(volume);
+    let mut stream_handle = DeviceSinkBuilder::open_default_sink()?;
+    stream_handle.log_on_drop(false);
+    let player = rodio::Player::connect_new(stream_handle.mixer());
+    player.set_volume(volume);
 
     // Load a sound from a file, using a path relative to Cargo.toml
     let file = BufReader::new(File::open(resource_path)?);
 
     // Decode that sound file into a source
-    let source = Decoder::new(file)?;
-    sink.append(source);
-    sink.sleep_until_end();
+    let source = Decoder::try_from(file)?;
+    player.append(source);
+    player.sleep_until_end();
     Ok(())
 }
